b346f62e731d51d50347a225cae87da017384b9f
MangoManguo <145375315+MangoManguo@users.noreply.github.com>
2023-12-12 10:08:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // SIMPLE ADDITION IPO CALCULATOR
 
-// Input
 document.getElementById("btn").addEventListener("click", calculate);
-// Process
 function calculate() {
+  // Input
   let number1 = document.getElementById("number1").value;
   let number2 = document.getElementById("number2").value;
   let number3 = document.getElementById("number3").value;
   let number4 = document.getElementById("number4").value;
   let number5 = document.getElementById("number5").value;
+  // Process
   let total = +number1 + +number2 + +number3 + +number4 + +number5;
   let average = total / 5;
+  // Output
   document.getElementById("grade").innerHTML = average;
 }
-// Output
+
